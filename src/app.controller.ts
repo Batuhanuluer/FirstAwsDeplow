c31147b4dde5dc9b7602cf9b44d843c703656ba1
BatuhanUluer <86664929+KayipTesla@users.noreply.github.com>
2024-05-12 21:32:29 +0300

    new message added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,6 +10,10 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Get('/pelin')
+  getPelin() : string {
+    return this.appService.getPeliniSev();
+  }
   @Post('/:token')
   returnWorld(@Param() Param){
     return Param.token;
